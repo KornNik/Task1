ddb21540fad2211558056a5e8105f38e27f5b20e
KornNik <kornienkokolya23@gmail.com>
2021-10-18 21:20:29 +0300

    Create alpha changing

---
+++ Assets/ChangedAlfa/Alpha.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Alpha : MonoBehaviour
+{
+
+    [SerializeField]private GameObject _gameObject;
+
+    private Color _colorFullAlpha = new Color(1, 1, 1, 1);
+    private Color _colorZeroAlpha = new Color(1, 1, 1, 0);
+    private float Speed = 1;
+    private Renderer _renderer;
+    private MaterialPropertyBlock _materialProperty;
+
+
+    void Start()
+    {
+        _renderer = transform.GetComponent<Renderer>();
+        _materialProperty = new MaterialPropertyBlock();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_gameObject.activeSelf)
+        {
+            _renderer.GetPropertyBlock(_materialProperty);
+            _materialProperty.SetColor("_Color", Color.Lerp(_colorFullAlpha, _colorZeroAlpha, Time.time * Speed / 5f));
+            _renderer.SetPropertyBlock(_materialProperty);
+        }
+    }
+}
---
+++ Assets/ImageShown.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using maxstAR;
+
+public class ImageShown : MonoBehaviour
+{
+    private Dictionary<string, ImageTrackableBehaviour> imageTrackablesMap =
+    new Dictionary<string, ImageTrackableBehaviour>();
+
+    private void Start()
+    {
+        TrackerManager.GetInstance().StartTracker(TrackerManager.TRACKER_TYPE_IMAGE);
+    }
+    void Update()
+    {
+    TrackingState state = TrackerManager.GetInstance().UpdateTrackingState();
+        TrackingResult trackingResult = state.GetTrackingResult();
+
+        for (int i = 0; i < trackingResult.GetCount(); i++)
+        {
+            Trackable trackable = trackingResult.GetTrackable(i);
+            imageTrackablesMap[trackable.GetName()].OnTrackSuccess(trackable.GetId(), trackable.GetName(), trackable.GetPose());
+        }
+    }
+
+    void OnApplicationPause(bool pause)
+    {
+        TrackerManager.GetInstance().StopTracker();
+    }
+
+    void OnDestroy()
+    {
+        TrackerManager.GetInstance().StopTracker();
+        TrackerManager.GetInstance().DestroyTracker();
+    }
+
+}
